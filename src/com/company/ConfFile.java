8e35c7c1d677ea8389aba4a70ae38fe73ef7094c
Ficusss-PC\Grigory <grisha.fioktistov@yandex.ru>
2017-11-16 05:49:36 +0400

    corrected reading of main config file

--- src/com/company/ConfFile.java
+++ src/com/company/ConfFile.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.io.File;
 import java.util.Scanner;
@@ -10,26 +11,21 @@ public class ConfFile {
 
     /**
      * Read config file and write data in Map.
-     * @param confFile - config file with data.
-     * @return confData - read data.
+     * @param configFile - config file with data.
+     * @return configData - read data.
      * @throws IOException - an exception occurs if the transferred file does not exist.
      * @throws RepetitionOfArgument - an exception occurs if config file include repetitive arguments.
      * @throws InvalidConfigurationData - an exception occurs if read data does not meet expectations.
      * @throws NumberFormatException - an exception occurs is thrown if the read field is not a number.
      */
-    public static Map read(File confFile) throws IOException, RepetitionOfArgument,
+    public static ArrayList<Map<String, String>> read(File configFile) throws IOException, RepetitionOfArgument,
             InvalidConfigurationData, NumberFormatException {
-        Scanner scanner = new Scanner(confFile);
+        Scanner scanner = new Scanner(configFile);
         StringBuffer argFromConfFile = new StringBuffer();
         while (scanner.hasNext()) {
             argFromConfFile.append(scanner.nextLine());
             argFromConfFile.append(" ");
         }
-        Map configData = Parser.getMap(argFromConfFile, Parser.REG_CONFIG_FILE);
-        if (configData.containsKey(ConfigParameter.COUNT_BIT.getValue())) {
-            configData.put(ConfigParameter.COUNT_BIT.getValue(),
-                    Integer.parseInt((String) configData.get(ConfigParameter.COUNT_BIT.getValue())));
-        }
-        return configData;
+        return Parser.getList(argFromConfFile, Parser.REG_MAIN_CONFIG_FILE);
     }
 }
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -2,17 +2,21 @@ package com.company;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.regex.*;
 
 public class Parser {
     static public String REG_COMMAND_LINE;
     static public String REG_CONFIG_FILE;
+    static public String REG_MAIN_CONFIG_FILE;
+
 
     /**
      * Static block with initialization of regular expressions.
      */
     static {
         REG_COMMAND_LINE = "(\\w+)=(\\w+\\.\\w+)";
+        REG_MAIN_CONFIG_FILE = "(\\w+)=(\\w+\\.\\w+)";
         REG_CONFIG_FILE = "(\\w+)=((\\W+)|(\\w+))";
     }
 
@@ -30,6 +34,24 @@ public class Parser {
             argStr.append(" ");
         }
         return argStr;
+    }
+
+    /**
+     * Get parsed string as like table.
+     * @param string - string to parsing.
+     * @param regStr - regular expression.
+     * @return list - list with parsed string.
+     */
+    public static ArrayList<Map<String, String>> getList(StringBuffer string, String regStr) {
+        ArrayList<Map<String, String>> list = new ArrayList<>();
+        Pattern p = Pattern.compile(regStr);
+        Matcher m = p.matcher(string);
+        while (m.find()) {
+            Map<String, String> tmp = new HashMap<>();
+            tmp.put(m.group(1), m.group(2));
+            list.add(tmp);
+        }
+        return list;
     }
 
     /**
